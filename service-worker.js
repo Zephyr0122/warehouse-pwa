8e322b9ffe15ac820aa40e2db1c63b9af4bb9582
Zephyr0122 <152155465+Zephyr0122@users.noreply.github.com>
2025-08-10 21:26:19 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,34 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('warehouse-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/service-worker.js',
-                '/icon.png'
-            ]);
-        })
-    );
+const CACHE_NAME = 'warehouse-pwa-cache-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request);
+    })
+  );
 });
